5fdea1c5674f05a9d4081fd5b0a4fffea96e0878
rémi  <remirobert33530@gmail.com>
2015-12-19 21:30:08 +0800

    update cell view

--- DataCell.js
+++ DataCell.js
@@ -21,12 +21,16 @@ class DataCell extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.nameShop}>
-          {this.state.coffeeShop.name}
-        </Text>
-        <Text style={styles.addressShop}>
-          {this.state.coffeeShop.address}
-        </Text>
+        <View style={styles.subContainer}>
+          <Text style={styles.nameShop}>
+            {this.state.coffeeShop.name}
+          </Text>
+          <Text style={styles.addressShop}
+            numberLines={0}>
+            {this.state.coffeeShop.address}
+          </Text>
+          <View style={styles.separator}/>
+        </View>
       </View>
     );
   }
@@ -39,16 +43,25 @@ var styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
+    padding: 10
   },
   nameShop: {
     height: 50,
-    fontSize: 15,
-    color: 'blue'
+    fontSize: 25,
+    color: 'blue',
+    marginBottom: 8
   },
   addressShop: {
     height: 50,
-    fontSize: 10,
-    color: 'blue'
+    fontSize: 15,
+    color: 'blue',
+  },
+  subContainer: {
+    flex: 1
+  },
+  separator: {
+    height: 1,
+    backgroundColor: 'darkGray'
   }
 });
 
